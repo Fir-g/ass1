50e564f27f1d63e4836752308f37ada0a768105a
Firoj Ahamad <firojahamad5072002@gmail.com>
2024-11-21 04:08:22 +0530

    Some change

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
+import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth-options";
-import nextAuth from "next-auth";
 
-export const handler = nextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
